11282333fad580e99e5880dc4b096929bd6a7542
mariahhau <135694401+mariahhau@users.noreply.github.com>
2024-01-10 15:11:21 +0200

    Update AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -7,7 +7,7 @@ export default function AboutPage() {
             <a href="https://www.movieofthenight.com/about/api"> Streaming Availability API by Movie of the Night</a>
 
             <br></br>
-            This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.
+            This website uses <a href="http://www.omdbapi.com/">OMDb API</a> but is not endorsed, certified, or otherwise approved by omdbapi.com.
         </>
 
     )
