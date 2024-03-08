b3906be4b022c621af86444a19f3e54077f77ef5
mariahhau <135694401+mariahhau@users.noreply.github.com>
2024-03-08 20:03:14 +0200

    Add searchParams to the url

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,23 +1,43 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import fetchData from "../service/ApiService";
 import Result from "../Result";
+import { useSearchParams } from "react-router-dom";
 
 interface Props {
   children?: ReactNode;
   setResults: (arr: Array<Result>) => void;
   setTitle: (title: string) => void;
+  q?: string | null;
 }
 
-
-function SearchBar({ setResults, setTitle }: Props) {
+function SearchBar({ setResults, setTitle, q }: Props) {
   const [input, setInput] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (q) {
+      getSearchResults(q);
+    }
+  }, []);
 
   const onFormSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const results = await fetchData(input.toLowerCase());
+    console.log("Form submit, fetchData call to ApiService from SearchBar");
+    getSearchResults(input.toLowerCase());
+    /*const results = await fetchData(input.toLowerCase());
     setTitle("Tulokset haulla " + input);
-    const results2 = removeDuplicants(results); 
+    const results2 = removeDuplicants(results);
     setResults(results2);
+    setSearchParams({ q: input });*/
+  };
+
+  const getSearchResults = async (keyword: string) => {
+    console.log("Get search results - searchbar");
+    const results = await fetchData(keyword);
+    setTitle("Tulokset haulla " + keyword);
+    const results2 = removeDuplicants(results);
+    setResults(results2);
+    setSearchParams({ q: keyword });
   };
 
   const handleChange = (value: string) => {
@@ -26,26 +46,27 @@ function SearchBar({ setResults, setTitle }: Props) {
   };
 
   return (
-      <form className="inline-flex search-form" onSubmit={onFormSubmit}>
-          <input
-            type="search"
-            className="form-control rounded-pill custom-search-input"
-            id="searchInput"
-            placeholder="Hae sarjoja tai elokuvia..."
-            value={input}
-            onChange={(e) => handleChange(e.target.value)}
-          />
-  
-          {<button type="submit" className="btn btn-secondary rounded-pill custom-search-button" onClick={() => onFormSubmit}>
-            <i className="fa fa-search search-icon"></i>
-           
-          </button>}
-        
-      </form>
-      
-    );
+    <form className="inline-flex search-form" onSubmit={onFormSubmit}>
+      <input
+        type="search"
+        className="form-control rounded-pill custom-search-input"
+        id="searchInput"
+        placeholder="Hae sarjoja tai elokuvia..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
 
-    
+      {
+        <button
+          type="submit"
+          className="btn btn-secondary rounded-pill custom-search-button"
+          onClick={() => onFormSubmit}
+        >
+          <i className="fa fa-search search-icon"></i>
+        </button>
+      }
+    </form>
+  );
 }
 
 export default SearchBar;
@@ -55,4 +76,4 @@ const removeDuplicants = (items: Array<Result>) => {
     return index === items.findIndex((o: any) => obj.imdbId === o.imdbId);
   });
   return unique;
-}
+};
