b8b2e7703b7c6b8b1567730ac1cf5e42873cf519
mariahhau <135694401+mariahhau@users.noreply.github.com>
2024-03-08 20:25:22 +0200

    Set the size of the card to remain unchanged when a button is clicked

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -5,9 +5,8 @@ import CardButton from "./CardButton";
 interface Props {
   item: Result;
   onSelectItem?: (item: Result) => void;
-  buttonType?: "add" | "delete" | "watched"
+  buttonType?: "add" | "delete" | "watched";
   buttonText?: string;
-
 }
 
 function MovieCard({ item, onSelectItem, buttonType, buttonText = "" }: Props) {
@@ -17,91 +16,90 @@ function MovieCard({ item, onSelectItem, buttonType, buttonText = "" }: Props) {
   const [hidden, setHidden] = useState(false);
 
   useEffect(() => {
-
     const fetchData = async () => {
-
-      const response = await fetch("http://localhost:4000/movieAPI/" + new URLSearchParams({ "id": item.imdbId }), {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      });
+      const response = await fetch(
+        "http://localhost:4000/movieAPI/" +
+          new URLSearchParams({ id: item.imdbId }),
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
       const json = await response.json();
       setDescription(json.Plot);
-      setImageSrc("https://picsum.photos/200/300")//json.Poster)
-    }
+      setImageSrc("https://picsum.photos/200/300"); //json.Poster)
+    };
 
-    fetchData()
-      .catch(console.error);
-  }, [])
-
+    fetchData().catch(console.error);
+  }, []);
 
   const getShortDescription = function () {
-
     let i = description.length >= 200 ? 200 : description.length;
     let char = description[i];
-    while (char !== ' ' && i > 0) {
+    while (char !== " " && i > 0) {
       i--;
       char = description[i];
     }
     return description.toString().slice(0, i) + "...";
-  }
+  };
 
   const deleteFromWatchlist = async () => {
-    const response = await fetch('http://localhost:4000/saved', {
-      method: 'DELETE',
+    const response = await fetch("http://localhost:4000/saved", {
+      method: "DELETE",
       body: JSON.stringify(item),
       headers: { "Content-Type": "application/json" },
-      credentials: 'include'
+      credentials: "include",
     });
     setHidden(true);
-    console.log(response)
-  }
+    console.log(response);
+  };
 
   const addToWatchlist = async () => {
-    const response = await fetch('http://localhost:4000/saved', {
-      method: 'POST',
+    const response = await fetch("http://localhost:4000/saved", {
+      method: "POST",
       body: JSON.stringify(item),
       headers: { "Content-Type": "application/json" },
-      credentials: 'include'
+      credentials: "include",
     });
-    console.log(response)
-  }
+    console.log(response);
+  };
 
   const addButtons = function () {
     if (!buttonType) return "";
     if (buttonType === "add") {
       return (
-      <div className="col-1">
-        <CardButton handleClick={addToWatchlist} text={buttonText} />
+        <div className="col-1">
+          <CardButton handleClick={addToWatchlist} text={buttonText} />
         </div>
-      )
-
+      );
     } else if (buttonType === "delete") {
       return (
         <div className="col-1">
           <CardButton handleClick={deleteFromWatchlist} text={buttonText} />
-          </div>
-        )
+        </div>
+      );
     }
-  }
+  };
 
   const getStreamingInfo = function (item: Result) {
     const unique = item.streamingInfo?.filter((obj: any, index) => {
-      return index === item.streamingInfo?.findIndex((o: any) => obj.service === o.service);
+      return (
+        index ===
+        item.streamingInfo?.findIndex((o: any) => obj.service === o.service)
+      );
     });
     return unique;
-  }
+  };
 
   if (hidden) return "";
   return (
     <li
-      className={
-        selected
-          ? "list-group-item active"
-          : "list-group-item"
-      }
+      className={selected ? "list-group-item active" : "list-group-item"}
       key={item.imdbId}
-      onClick={() => {
-        setSelected(!selected);
+      onClick={(e) => {
+        if (e.target instanceof HTMLButtonElement == false) {
+          setSelected(!selected);
+        }
         if (onSelectItem) onSelectItem(item);
       }}
       value={JSON.stringify(item)}
@@ -111,59 +109,75 @@ function MovieCard({ item, onSelectItem, buttonType, buttonText = "" }: Props) {
           <div className="card text-white bg-primary mb-3">
             <div className="row gx-1">
               <div className="col-12 col-md-4">
-                <img src={imageSrc} alt="" className={selected ? "card-img-top rounded-float-left" : "card-img-top rounded-float-left poster-closed"} loading="lazy" />
+                <img
+                  src={imageSrc}
+                  alt=""
+                  className={
+                    selected
+                      ? "card-img-top rounded-float-left"
+                      : "card-img-top rounded-float-left poster-closed"
+                  }
+                  loading="lazy"
+                />
               </div>
               <div className="col">
                 <div className="card-body">
-
                   <h5 className="card-title">{item.title}</h5>
-                  <h6 className="card-subtitle text-muted"> {item.startYear + "-" + item.endYear + " " + item.type}</h6>
+                  <h6 className="card-subtitle text-muted">
+                    {" "}
+                    {item.startYear + "-" + item.endYear + " " + item.type}
+                  </h6>
                   <p className="card-text">
-
                     {selected ? description : getShortDescription()}
                   </p>
                   <p className="fs-6">Katsottavissa:</p>
                   <ul className="list-group list-group-horizontal">
-
-                    {item.streamingInfo.length > 0 ? getStreamingInfo(item).map((item: any) => <li className="list-group-item" key={item.service}><a href={item.link}>{item.service}</a></li>) : "-"}
+                    {item.streamingInfo.length > 0
+                      ? getStreamingInfo(item).map((item: any) => (
+                          <li className="list-group-item" key={item.service}>
+                            <a href={item.link}>{item.service}</a>
+                          </li>
+                        ))
+                      : "-"}
                   </ul>
                   <p className="card-text">
-                    <small className="text-muted">Päivitetty {formatDHMS(msToDHMS(Date.now() - parseInt(item.fetchedAt)))} sitten</small>
+                    <small className="text-muted">
+                      Päivitetty{" "}
+                      {formatDHMS(
+                        msToDHMS(Date.now() - parseInt(item.fetchedAt))
+                      )}{" "}
+                      sitten
+                    </small>
                   </p>
-
-                </div></div>
-
+                </div>
+              </div>
             </div>
-
           </div>
         </div>
         {addButtons()}
-
       </div>
     </li>
-  )
+  );
 }
-
-
 
 export default MovieCard;
 
 const msToDHMS = function (ms: number) {
-  const days = Math.floor((ms) / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((ms) / 1000 / 60 / 60 % 24);
-  const minutes = Math.floor((ms) / 1000 / 60 % 60);
-  const seconds = Math.floor((ms) / 1000 % 60);
+  const days = Math.floor(ms / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((ms / 1000 / 60 / 60) % 24);
+  const minutes = Math.floor((ms / 1000 / 60) % 60);
+  const seconds = Math.floor((ms / 1000) % 60);
 
   return [
     days.toString().padStart(2, "0"),
     hours.toString().padStart(2, "0"),
     minutes.toString().padStart(2, "0"),
-    seconds.toString().padStart(2, "0")
+    seconds.toString().padStart(2, "0"),
   ].join(":");
-}
+};
 
 const formatDHMS = function (dhms: string) {
-  console.log(dhms)
+  console.log(dhms);
   const parts = dhms.split(":", 4);
   const text = ["päivää", "tuntia ", "minuuttia", "sekuntia"];
 
@@ -173,6 +187,4 @@ const formatDHMS = function (dhms: string) {
     }
   }
   return "muutama sekunti";
-}
-
-
+};
