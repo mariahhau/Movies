db99499e007c4e28cfc9833390f4bc15c0afab1e
mariahhau <135694401+mariahhau@users.noreply.github.com>
2024-03-09 10:27:57 +0200

    Update AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,14 +1,17 @@
+import Header from "../Header";
 
 export default function AboutPage() {
-    return (
-        <>
-            The streaming availability information is provided by
-
-            <a href="https://www.movieofthenight.com/about/api"> Streaming Availability API by Movie of the Night</a>
-
-            <br></br>
-            This website uses <a href="http://www.omdbapi.com/">OMDb API</a> but is not endorsed, certified, or otherwise approved by omdbapi.com.
-        </>
-
-    )
+  return (
+    <>
+      <Header />
+      The streaming availability information is provided by
+      <a href="https://www.movieofthenight.com/about/api">
+        {" "}
+        Streaming Availability API by Movie of the Night
+      </a>
+      <br></br>
+      This website uses <a href="http://www.omdbapi.com/">OMDb API</a> but is
+      not endorsed, certified, or otherwise approved by omdbapi.com.
+    </>
+  );
 }
