15141f5fbc973fa4b6e73ec8220a6166652ab029
mariahhau <135694401+mariahhau@users.noreply.github.com>
2024-03-14 19:07:18 +0200

    Display password as dots

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,37 +1,41 @@
 import { useState } from "react";
 
-export default function RegisterPage () {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+export default function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    async function register(ev:any){
-        ev.preventDefault();
+  async function register(ev: any) {
+    ev.preventDefault();
 
-        const response = await fetch('http://localhost:4000/register', {
-            method: 'POST',
-            body: JSON.stringify({username,password}),
-            headers: {'Content-Type':'application/json'},
-        });
-        if (response.status === 200) {
-            alert('Registration successful');
-        } else {
-            alert('Registration failed');
-
-        }
+    const response = await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status === 200) {
+      alert("Registration successful");
+    } else {
+      alert("Registration failed");
     }
-    return (
-        <form className="register" onSubmit={register}>
-            <h1>Register</h1>
-            <input type="text" 
-                placeholder="username" 
-                value={username}
-                onChange={ev => setUsername(ev.target.value)}/>
-            <input type="text" 
-                placeholder="password"
-                value={password}
-                onChange={ev => setPassword(ev.target.value)}/>
-            <button>Register</button>
-        </form>
-    )
-
+  }
+  return (
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="username"
+        autoComplete="username"
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        autoComplete="new-password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button>Register</button>
+    </form>
+  );
 }
